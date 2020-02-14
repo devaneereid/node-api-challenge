a4e3f7160a0ea1e4f0459bf30b4736d70d3f17b3
Devanee Reid <devaneereid@gmail.com>
2020-02-14 10:26:57 -0700

    delete post and put request working

--- routers/actionRouters.js
+++ routers/actionRouters.js
@@ -4,7 +4,7 @@ const Projects = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
-// GET Request (working)
+// GET Request 
 router.get('/', (req, res) => {
     Actions.get(req.query)
            .then(action => {
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
            });
 });
 
-// GET ID Request (working)
+// GET ID Request 
 router.get('/:id', (req, res) => {
     Actions.get(req.params.id)
            .then(user => {
@@ -28,6 +28,55 @@ router.get('/:id', (req, res) => {
            });
 });
 
+// DELETE Request
+router.delete('/:id', (req, res) => {
+    Actions.remove(req.params.id)
+            .then(count => {
+                if (count > 0) {
+                    res.status(200).json({ message: "The user has been nuked" })
+                } else {
+                    res.status(404).json({ message: "The user could not be found" })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ message: "Error deleting the user" })
+            });
+});
+
+// PUT Request 
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    Actions.update(id, req.body)
+            .then(user => {
+                if (user) {
+                    res.status(201).json({...req.body, id: id})
+                } else {
+                    res.status(404).json({ message: "Error finding user" })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+                res.status(500).json({ message: "Error updating user" })
+            });
+
+});
+
+// POST Request
+router.post('/', (req, res) => {
+    Actions.insert(req.body) 
+            .then(action => {
+                if(action) {
+                    res.status(201).json(action)
+                } else {
+                    res.status(400).json({ message: "Error finding action" })
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({ message: "Error posting action"})
+            });
+});
 
 
 
